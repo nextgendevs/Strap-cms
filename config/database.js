102abaceadf2488905ee01cdb4304b3a72bb8dee
Himank Jain <himankjain@vivaldi.net>
2020-09-06 13:25:07 +0530

    POSTgres

--- config/database.js
+++ config/database.js
@@ -1,3 +1,4 @@
+/*
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
@@ -9,6 +10,27 @@ module.exports = ({ env }) => ({
       },
       options: {
         useNullAsDefault: true,
+      },
+    },
+  },
+});
+*/
+
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'postgres',
+        host: env('DATABASE_HOST', '127.0.0.1'),
+        port: env.int('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME', 'strapi'),
+        username: env('DATABASE_USERNAME', ''),
+        password: env('DATABASE_PASSWORD', ''),
+      },
+      options: {
+        ssl: false,
       },
     },
   },
